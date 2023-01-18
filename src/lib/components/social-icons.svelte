fbba2d7345d710d91f9461717e172f2bf13657bf
blacksansa1 <atendimento.simpleti@gmail.com>
2023-01-18 08:57:26 -0300

    social links

--- src/lib/components/social-icons.svelte
+++ src/lib/components/social-icons.svelte
@@ -1,13 +1,25 @@
 <div class="container">
-    <img src="/instagram.svg" alt="" class="icon" />
-    <img src="/twitter.svg" alt="" class="icon" />
-    <img src="/whatsapp.svg" alt="" class="icon" />
-    <img src="/facebook.svg" alt="" class="icon" />
+    <a href="https://www.instagram.com/simple__ti/">
+        <img src="/instagram.svg" alt="" />
+    </a>
+    <a href="https://twitter.com/simple__ti">
+        <img src="/twitter.svg" alt=""  />
+    </a>
+    <a href="https://wa.me/5524993321159?text=Estou+com+um+problema">
+        <img src="/whatsapp.svg" alt=""  />
+    </a>
+    <a href="https://www.facebook.com/Simple-TI-116247424692536">
+        <img src="/facebook.svg" alt=""  />
+    </a>
+    
+    
+    
+    
 </div>
 
 
 <style>
-.icon {
+a {
     margin-left: 5px
 }
 </style>
